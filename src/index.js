1530ab8c7d2ee6baf18d4a58cb82c4ff718394a0
Julian Ehiem <jules.ehiem@gmail.com>
2020-12-29 16:07:49 -0500

    init commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+class Square extends React.Component {
+  render() {
+    return (
+      <button className = "square">
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className = "game">
+        <div className = "game-board">
+          <Board />
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render (
+  <Game />,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
