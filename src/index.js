8ecd800d741502e09c48d54b4d1790101c040844
Julian Ehiem <jules.ehiem@gmail.com>
2020-12-30 17:43:36 -0500

    added functional components

--- src/index.js
+++ src/index.js
@@ -4,24 +4,16 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  render() {
+function Square(props) {
     return (
       <button 
       className = "square" 
-      onClick = {() => this.setState({value: 'X'})}
+      onClick = {props.onClick}
       >
-        {this.state.value}
+        {props.value}
       </button>
     );
-  }
-}
+  };
 
 class Board extends React.Component {
   constructor(props) {
@@ -30,8 +22,19 @@ class Board extends React.Component {
       squares: Array(9).fill(null),
     }
   }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
   renderSquare(i) {
-    return <Square value ={this.state.squares[i]} />;
+    return (
+    <Square 
+      value ={this.state.squares[i]}
+      onClick = {() => this.handleClick(i)}
+
+    />
+    );
   }
   render() {
     const status = 'Next Player X';
