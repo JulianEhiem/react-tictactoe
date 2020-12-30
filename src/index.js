7b5b4a4a982b2de142577f033cbf65d029da90a0
Julian Ehiem <jules.ehiem@gmail.com>
2020-12-30 06:28:51 -0500

    made squares interactive: added onClick()

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 class Square extends React.Component {
   render() {
     return (
-      <button className = "square">
+      <button className = "square" onClick = {() => alert("Click")}>
         {this.props.value}
       </button>
     );
@@ -18,6 +18,30 @@ class Board extends React.Component {
   renderSquare(i) {
     return <Square value={i} />;
   }
+  render() {
+    const status = 'Next Player X';
+
+    return (
+      <div>
+        <div className = "status">{status}</div>
+        <div className = "board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className = "board-row">
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+        </div>
+        <div className = "board-row">
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+        </div>
+      </div>
+    );
+  }
 }
 
 class Game extends React.Component {
@@ -26,6 +50,10 @@ class Game extends React.Component {
       <div className = "game">
         <div className = "game-board">
           <Board />
+        </div>
+        <div className = "game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
         </div>
       </div>
     )
