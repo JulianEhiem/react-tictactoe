4861f0a5e7fe42d4329ff86e759952844262c8da
Julian Ehiem <jules.ehiem@gmail.com>
2020-12-30 07:23:58 -0500

    added state to Board class

--- src/index.js
+++ src/index.js
@@ -5,18 +5,33 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 class Square extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      <button className = "square" onClick = {() => alert("Click")}>
-        {this.props.value}
+      <button 
+      className = "square" 
+      onClick = {() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value ={this.state.squares[i]} />;
   }
   render() {
     const status = 'Next Player X';
